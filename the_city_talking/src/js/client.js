fd6084a26c62a232357c738f8e367d0aab774ca3
Nuclehon <benjamin.deslandes@etu.univ-nantes.fr>
2016-01-31 19:26:53 +0100

    [WIP] Debugged client using generated JSON

--- the_city_talking/src/js/client.js
+++ the_city_talking/src/js/client.js
@@ -4,17 +4,15 @@ import "../scss/main.scss";
 var width=1000,
 	height=400;
 
+var data=[];
+
+var testmonth="01-2014";
+
 var chart = d3.select(".chart")
 	.attr("width",width)
 	.attr("height",height);
 
 var formatDate = d3.time.format("%d-%m-%Y");
-
-function type(d) {
-  d.date = formatDate.parse(d.date);
-  d.no2 = +d.no2;
-  return d;
-}
 
 var x = d3.time.scale()
 	.range([0,width]);
@@ -23,14 +21,26 @@ var y = d3.scale.linear()
 	.range([height,0]);
 
 var line = d3.svg.line()
-    .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d.no2); });
+    .x(function(d) { return x(d.Date); })
+    .y(function(d) { return y(d.NO2); });
 
-d3.csv("per_day_test.csv",type,function(error,data){
+function drawmonth(month)
+{
+	d3.json("json_centre/"+testmonth+".json",function(error,json){
+
+	});
+	d3.select("path").datum();
+}
+
+d3.json("json_centre/"+testmonth+".json",function(error,json){
 	if (error) throw error;
+	data.Date = json.map(function(d){return formatDate.parse(d.Date)});
+	data.NO2 = json.map(function(d){return +d.NO2});
 
-	x.domain(d3.extent(data, function(d) { return d.date; }));
-	y.domain(d3.extent(data, function(d) { return d.no2; }));
+	console.log(data);
+
+	x.domain(d3.extent(data, function(d) { return d.Date; }));
+	y.domain(d3.extent(data, function(d) { return d.NO2; }));
 
 	chart.append("path")
 		.datum(data)
@@ -38,5 +48,5 @@ d3.csv("per_day_test.csv",type,function(error,data){
 		.attr("class","lineb")
 		.transition()
 		.delay(500)
-		.attr("class","line")
+		.attr("class","line");
 });
